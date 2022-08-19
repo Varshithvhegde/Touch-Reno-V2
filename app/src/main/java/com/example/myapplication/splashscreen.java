638173300686d35df475c109daf65d5a0b2ba35a
Varshithvhegde <varshithvh@gmail.com>
2022-08-19 18:15:22 +0530

    optimised the imports

--- app/src/main/java/com/example/myapplication/splashscreen.java
+++ app/src/main/java/com/example/myapplication/splashscreen.java
@@ -2,13 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.WindowManager;
-import android.widget.VideoView;
+
 
 public class splashscreen extends AppCompatActivity {
 
