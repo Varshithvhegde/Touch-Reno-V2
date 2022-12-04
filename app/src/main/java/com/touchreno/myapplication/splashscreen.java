db3d30d27a168ff0b202be903239f33e40218b12
Varshithvhegde <varshithvh@gmail.com>
2022-12-04 18:41:31 +0530

    Added checking network checking

--- app/src/main/java/com/touchreno/myapplication/splashscreen.java
+++ app/src/main/java/com/touchreno/myapplication/splashscreen.java
@@ -1,20 +1,28 @@
 package com.touchreno.myapplication;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class splashscreen extends AppCompatActivity {
+public class splashscreen extends AppCompatActivity implements ConnectionReceiver.ReceiverListener {
     FirebaseAuth auth;
     private static int SPLASH_SCREEN_TIME_OUT=2000;
     ProgressBar progressBar;
+    boolean isConnected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,21 +30,72 @@ public class splashscreen extends AppCompatActivity {
         setContentView(R.layout.activity_splashscreen);
         progressBar=(ProgressBar) findViewById(R.id.progressbar);
         progressBar.setVisibility(View.VISIBLE);
+        checkConnection();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    // Start home activity
-                    startActivity(new Intent(splashscreen.this, MainActivity.class));
-                    finish();
-                }
-                else{
-                    startActivity(new Intent(splashscreen.this, LoginActivity.class));
-                    finish();
+                if(isConnected) {
+                    if (user != null) {
+                        // User is signed in
+                        // Start home activity
+                        startActivity(new Intent(splashscreen.this, MainActivity.class));
+                        finish();
+                    } else {
+                        startActivity(new Intent(splashscreen.this, LoginActivity.class));
+                        finish();
+                    }
                 }
             }
         },SPLASH_SCREEN_TIME_OUT);
+    }
+
+    private void checkConnection() {
+        IntentFilter intentFilter = new IntentFilter();
+
+        // add action
+        intentFilter.addAction("android.new.conn.CONNECTIVITY_CHANGE");
+
+        // register receiver
+        registerReceiver(new ConnectionReceiver(), intentFilter);
+
+        // Initialize listener
+        ConnectionReceiver.Listener =  this;
+
+        // Initialize connectivity manager
+        ConnectivityManager manager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Initialize network info
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        // get connection status
+         isConnected = networkInfo != null && networkInfo.isConnectedOrConnecting();
+
+        // display snack bar
+        showSnackBar(isConnected);
+    }
+
+    private void showSnackBar(boolean isConnected) {
+        if (isConnected) {
+//            Toast.makeText(this, "Network is there", Toast.LENGTH_SHORT).show();
+        }
+        else{
+           AlertDialog.Builder builder= new AlertDialog.Builder(this);
+           builder.setMessage("Please Check Your Internet Connection")
+            .setCancelable(false)
+            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                        System.exit(0);
+                }
+            });
+           AlertDialog alert=builder.create();
+           alert.show();
+        }
+    }
+
+    @Override
+    public void onNetworkChange(boolean isConnected) {
+        showSnackBar(isConnected);
     }
 }
