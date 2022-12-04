3a36abf237fa3d774f9520fbe19ca19659ec34db
Varshithvhegde <varshithvh@gmail.com>
2022-12-04 19:26:10 +0530

    Added Playstore update checking message to app

--- app/src/main/java/com/touchreno/myapplication/MainActivity.java
+++ app/src/main/java/com/touchreno/myapplication/MainActivity.java
@@ -6,8 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.touchreno.myapplication.adapters.ImageSliderAdapter;
 import com.google.android.material.navigation.NavigationView;
 
@@ -33,12 +40,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public LinearLayout navcart;
     SliderView sliderView;
     AlertDialog.Builder builder;
+    FirebaseDatabase database;
+    FirebaseAuth auth;
+    Integer val;
     int[] images={R.drawable.slide1,R.drawable.slide2,R.drawable.slide3,R.drawable.slide4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+
+
+
+
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         navcart=(LinearLayout) findViewById(R.id.cartnav);
@@ -65,6 +81,50 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
         builder=new AlertDialog.Builder(this);
         firebaseAuth=FirebaseAuth.getInstance();
+
+        auth=FirebaseAuth.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        database=FirebaseDatabase.getInstance();
+        DatabaseReference ref= database.getReference().child("Flag");
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                val = snapshot.getValue(Integer.class);
+                if(val==1){
+                    checkplayUpdate();
+                }
+               
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+    }
+
+    private void checkplayUpdate() {
+        androidx.appcompat.app.AlertDialog.Builder builder= new androidx.appcompat.app.AlertDialog.Builder(this);
+        builder.setMessage("New App Update")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        System.exit(0);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//
+                    }
+                });
+        androidx.appcompat.app.AlertDialog alert=builder.create();
+        alert.show();
     }
 
     @Override
